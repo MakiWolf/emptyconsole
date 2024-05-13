d86dfab15b890c53f97430f2b5f0f5b9e2bc55d4
makiwolf <makiwolf@outlook.at>
2024-05-13 23:22:42 +0200

    add files

---
+++ dotdeveloptest/dotdeveloptest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotdeveloptest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
